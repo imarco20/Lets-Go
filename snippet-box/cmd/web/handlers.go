9f2c1f4a8f8c7d191225e5eda735bb54104ec287
MarcoMounir21 <mam@ext.avian.dk>
2021-07-22 15:23:09 +0200

    add code for Chapter 4 of the book on Database-Driven Responses

--- snippet-box/cmd/web/handlers.go
+++ snippet-box/cmd/web/handlers.go
@@ -5,12 +5,25 @@ import (
 	"html/template"
 	"net/http"
 	"strconv"
+
+	"marcode.io/snippetbox/pkg/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		app.notFound(w)
 		return
+	}
+
+	s, err := app.snippets.Latest()
+	
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	for _, snippet := range s {
+		fmt.Fprintf(w, "%v\n", snippet)
 	}
 
 	files := []string{
@@ -20,39 +33,59 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ts, err := template.ParseFiles(files...)
-	
+
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
-	
+
 	err = ts.Execute(w, nil)
-	
+
 	if err != nil {
 		app.serverError(w, err)
 	}
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	
+
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	
+
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
 	}
-	
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+
+	s, err := app.snippets.Get(id)
+
+	if err == models.ErrNoRecord {
+		app.notFound(w)
+		return
+	} else if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	fmt.Fprintf(w, "%v", s)
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	
+
 	if r.Method != "POST" {
 		w.Header().Set("Allow", "POST")
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	
-	w.Write([]byte("Create a new snippet..."))
+
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi"
+	expires := "7"
+
+	id, err := app.snippets.Insert(title, content, expires)
+
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
 }
-	
--- snippet-box/cmd/web/routes.go
+++ snippet-box/cmd/web/routes.go
@@ -3,7 +3,7 @@ package main
 import "net/http"
 
 func (app *application) routes() *http.ServeMux {
-	
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
