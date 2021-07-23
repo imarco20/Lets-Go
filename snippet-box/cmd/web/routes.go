ee1fc5b12f00eb1e662cd210fc90665d4196ec9c
MarcoMounir21 <mam@ext.avian.dk>
2021-07-23 13:55:57 +0200

    add code for chapter 6 on Middleware

--- snippet-box/cmd/web/routes.go
+++ snippet-box/cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
@@ -12,5 +12,6 @@ func (app *application) routes() *http.ServeMux {
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	return mux
+	// wrap the servermux with the secureHeaders middleware
+	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
 }
