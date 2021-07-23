beec4a80ddf09acd20929f5209c1b5db98694025
MarcoMounir21 <mam@ext.avian.dk>
2021-07-23 12:20:27 +0200

    add code for chapter 5 on Dynamic HTML Templates

--- snippet-box/cmd/web/handlers.go
+++ snippet-box/cmd/web/handlers.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"html/template"
+	// "html/template"
 	"net/http"
 	"strconv"
 
@@ -16,34 +16,34 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	}
 
 	s, err := app.snippets.Latest()
-	
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
-
-	for _, snippet := range s {
-		fmt.Fprintf(w, "%v\n", snippet)
-	}
-
-	files := []string{
-		"./ui/html/home.page.tmpl",
-		"./ui/html/base.layout.tmpl",
-		"./ui/html/footer.partial.tmpl",
-	}
-
-	ts, err := template.ParseFiles(files...)
 
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
 
-	err = ts.Execute(w, nil)
+	app.render(w, r, "home.page.tmpl", &templateData{Snippets: s})
 
-	if err != nil {
-		app.serverError(w, err)
-	}
+	// data := &templateData{Snippets: s}
+
+	// files := []string{
+	// 	"./ui/html/home.page.tmpl",
+	// 	"./ui/html/base.layout.tmpl",
+	// 	"./ui/html/footer.partial.tmpl",
+	// }
+
+	// ts, err := template.ParseFiles(files...)
+
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// 	return
+	// }
+
+	// err = ts.Execute(w, data)
+
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// }
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
@@ -65,7 +65,29 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "%v", s)
+	app.render(w, r, "show.page.tmpl", &templateData{Snippet: s})
+
+	// data := &templateData{Snippet: s}
+
+	// files := []string{
+	// 	"./ui/html/show.page.tmpl",
+	// 	"./ui/html/base.layout.tmpl",
+	// 	"./ui/html/footer.partial.tmpl",
+	// }
+
+	// ts, err := template.ParseFiles(files...)
+
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// 	return
+	// }
+
+	// err = ts.Execute(w, data)
+
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// }
+
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
--- snippet-box/pkg/models/mysql/snippets.go
+++ snippet-box/pkg/models/mysql/snippets.go
@@ -47,7 +47,7 @@ func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
 }
 
 func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
-	
+
 	stmt := `SELECT id, title, content, created, expires FROM snippets
 	WHERE expires > UTC_TIMESTAMP() ORDER BY created DESC LIMIT 10`
 
@@ -62,7 +62,7 @@ func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
 	snippets := []*models.Snippet{}
 
 	for rows.Next() {
-		
+
 		s := &models.Snippet{}
 
 		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
